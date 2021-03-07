b82da40417f3c4842db2edb296ff6994e821e9b6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2021-03-07 09:31:52 +0100

    astar_mirror/ardumont: Format alignment

--- keyboards/atreus/keymaps/ardumont/keymap.c
+++ keyboards/atreus/keymaps/ardumont/keymap.c
@@ -32,9 +32,9 @@
    */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,     KC_U,    KC_I,    KC_O,    KC_P    ,
-    CTL_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,     KC_J,    KC_K,    KC_L,    CTL_SCLN ,
-    SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,     KC_M,    KC_COMM, KC_DOT,  SFT_SLSH ,
+    KC_Q,   KC_W,   KC_E,     KC_R,    KC_T,                       KC_Y,     KC_U,    KC_I,    KC_O,    KC_P    ,
+    CTL_A,  KC_S,   KC_D,     KC_F,    KC_G,                       KC_H,     KC_J,    KC_K,    KC_L,    CTL_SCLN ,
+    SFT_Z,  KC_X,   KC_C,     KC_V,    KC_B,                       KC_N,     KC_M,    KC_COMM, KC_DOT,  SFT_SLSH ,
     KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, ALT_SPC,  ALT_SPC, ALT_SPC, ALT_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
 

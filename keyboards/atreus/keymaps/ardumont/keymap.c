86b82549146e5084591fb03561d314ac3e60df00
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2021-03-07 09:17:29 +0100

    astar_mirror/ardumont: Replace backspace with spc-alt key

--- keyboards/atreus/keymaps/ardumont/keymap.c
+++ keyboards/atreus/keymaps/ardumont/keymap.c
@@ -25,39 +25,39 @@
 #define SFT_SLSH MT(MOD_RSFT, KC_SLSH)
 
   /*
-   *  q     w     e     r     t           ||         y      u     i     o  p
-   *  a/ctl s     d     f     g           ||         h      j     k     l  ;/ctl
-   *  z/sft x     c     v     b           ||         n      m     ,     .  //sft
-   *  esc   tab  super shift bksp alt/spc || alt/spc space  fn    -     '  enter
+   *  q     w     e     r     t              ||         y        u     i/tab o  p
+   *  a/ctl s     d     f     g              ||         h        j     k     l  ;/ctl
+   *  z/sft x     c     v     b              ||         n        m     ,     .  //sft
+   *  esc   tab  super shift alt/spc alt/spc || alt/spc alt/spc  fn    -     '  enter
    */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    CTL_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    CTL_SCLN ,
-    SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  SFT_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  ALT_SPC, ALT_SPC, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,     KC_U,    KC_I,    KC_O,    KC_P    ,
+    CTL_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,     KC_J,    KC_K,    KC_L,    CTL_SCLN ,
+    SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,     KC_M,    KC_COMM, KC_DOT,  SFT_SLSH ,
+    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, ALT_SPC,  ALT_SPC, ALT_SPC, ALT_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
 
   /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
+   *  !       @     up     {    }              ||         pgup      7     8     9    *
+   *  #     left   down  right  $              ||         pgdn      4     5     6    +
+   *  [       ]      (     )    &              ||           `       1     2     3    \
+   * lower  insert super shift alt/spc alt/spc || alt/spc alt/spc   fn    .     0    =
    */
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, ALT_SPC, ALT_SPC, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, ALT_SPC, ALT_SPC, ALT_SPC, ALT_SPC, KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
   /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
+   * insert home   up  end   pgup             ||          up       F7  F8    F9     F10
+   *  del   left  down right pgdn             ||         down      F4  F5    F6     F11
+   *       volup             reset            ||                   F1  F2    F3     F12
+   *       voldn  super shift alt/spc alt/spc || alt/spc alt/spc   L0  prtsc scroll pause
    */
   [_LW] = LAYOUT( /* [> LOWER <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, ALT_SPC, ALT_SPC, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
+    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,    KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
+    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN,  KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
+    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,    KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
+    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, ALT_SPC, ALT_SPC, ALT_SPC, ALT_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
 };

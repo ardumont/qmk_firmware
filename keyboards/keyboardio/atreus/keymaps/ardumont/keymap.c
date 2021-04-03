e66d7c5ebac9a31893f3657deb6fdbb3a8951110
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2021-04-03 12:25:00 +0200

    keyboardio/atreus: Decrease a bit the tapping term time for a/ctrl
    Feels sensible enough.

--- keyboards/keyboardio/atreus/keymaps/ardumont/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/ardumont/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case A_CTL:
-            return TAPPING_TERM + 100;
+            return TAPPING_TERM + 75;
         default:
             return TAPPING_TERM;
     }

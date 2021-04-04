69e0a34c00791078be6748910075e84f14974507
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2021-04-04 14:58:27 +0200

    keyboardio/atreus: Update copyright headers

--- keyboards/keyboardio/atreus/keymaps/ardumont/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/ardumont/keymap.c
@@ -1,4 +1,5 @@
 // Copyright (C) 2019, 2020  Keyboard.io, Inc
+//               2021  Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
 //
 // this is the style you want to emulate.
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,

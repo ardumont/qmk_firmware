4ea295eae0b99964a723a40fbfbd324dceebb03f
Antoine R. Dumont (@ardumont) <ardumont@duck.com>
2024-11-29 12:05:47 +0100

    ardumont/keymap: Wrap comment lines

--- keyboards/keyboardio/atreus/keymaps/ardumont/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/ardumont/keymap.c
@@ -1,21 +1,21 @@
 // Copyright (C) 2019, 2020  Keyboard.io, Inc
 //               2021  Antoine R. Dumont (@ardumont) <ardumont@duck.com>
 //
-// this is the style you want to emulate.
-// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+// this is the style you want to emulate. This is the canonical layout file for the
+// Quantum project. If you want to add another keyboard,
 
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
+// Each layer gets a name for readability, which is then used in the keymap matrix
+// below. The underscores don't mean anything - you can have a layer called STUFF or any
+// other name. Layer names don't all need to be of the same length, obviously, and you
+// can also skip them entirely and just use numbers.
 enum layer_names {
     _QW,
     _RS,
     _LW,
 };
-
+?
 // tap: z    // hold: SHIFT
 #define Z_SFT     SFT_T(KC_Z)
 // tap: /    // hold: SHIFT
